4e31b54c463b2dbb5a93c0c76bacb785e491a118
karlancheta <karl.josephancheta@gmail.com>
2024-11-13 23:31:22 -0600

    changed

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ chocolates = [
     {
         "name": "White Chocolate",
         "image": "https://m.media-amazon.com/images/I/61-pg5u+9pL.jpg",
-        "price": 6.49
+        "price": 15.49
     }
 ]
 
